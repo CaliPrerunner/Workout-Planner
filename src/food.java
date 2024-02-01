f15c663c707a3be164ce97fbaa1bd986936ed87f
CaliPrerunner <marco@marcorobles.com>
2024-02-01 11:29:57 -0800

    first commit

---
+++ src/food.java
@@ -0,0 +1,23 @@
+public class food {
+
+    float protien;
+    float totalCarbohydrates;
+    float fat;
+    float calories;
+    float grams;
+    int barCode;
+    float totalsugars;
+    float fiber;
+    float transfat; //verb bad
+    float sodium; //typically mg
+    float addedSugars;
+
+
+
+
+
+    public food(){
+
+
+    }
+}
